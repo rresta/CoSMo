33a4df3c29ba5ebabc3853a04335db943f2c16ef
rresta <s222385@studenti.polito.it>
2017-09-27 23:45:23 +0200

    Version with Argument Parser and verbose Options.

---
+++ include/option.h
@@ -0,0 +1,314 @@
+// ==========================================================================
+//               LaRAgu - Lagrangian Relaxation Aligner GU
+// ==========================================================================
+// Copyright (c) 2015-2016, Gianvito Urgese
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Gianvito Urgese nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL GIANVITO URGESE OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+// Author: Gianvito Urgese <gianvito.urgese@polito.it>
+// ==========================================================================
+// This file contains the option structures and functions of seqan_laragu
+// application.
+// ==========================================================================
+
+#ifndef INCLUDE_OPTION_H_
+#define INCLUDE_OPTION_H_
+
+// ----------------------------------------------------------------------------
+// std headers
+// ----------------------------------------------------------------------------
+
+#include <iostream>
+#include <string.h>
+
+// ----------------------------------------------------------------------------
+// SeqAn headers
+// ----------------------------------------------------------------------------
+
+#include <seqan/arg_parse.h>
+
+// ----------------------------------------------------------------------------
+// App headers
+// ----------------------------------------------------------------------------
+
+// defines all the constants used in the app
+//#include "data_types.h"
+
+// ============================================================================
+// Macro utility
+// ============================================================================
+
+#define _V(_opt, _str) {if(_opt.verbose>0) std::cerr << _str << std::endl;}
+#define _VV(_opt, _str) {if(_opt.verbose>1) std::cerr << _str << std::endl;}
+#define _VVV(_opt, _str) {if(_opt.verbose>2) std::cerr << _str << std::endl;}
+
+//using namespace std;
+
+// ============================================================================
+// Functors
+// ============================================================================
+
+typedef EqualsChar<'.'>        IsDot;
+typedef EqualsChar<'/'>        IsSlash;
+typedef EqualsChar<'\\'>       IsBackSlash;
+
+#ifdef PLATFORM_WINDOWS
+typedef IsBackSlash        IsPathDelimited;
+#else
+typedef IsSlash            IsPathDelimited;
+#endif
+
+// ----------------------------------------------------------------------------
+// Class Options
+// ----------------------------------------------------------------------------
+//TODO this can be re-written as a set of three maps with keys and values but
+//if in the future should be embedded in the parsing function is better to
+//conserve this style
+struct Options
+{
+// Name of input files
+    seqan::CharString inFile;
+// Name of input fileShape
+//    seqan::CharString inFileShape;
+// Name of output file (default: stdout)
+    seqan::CharString outFile;
+// temporary directory where to save intermediate files. Default: use the input file directory.
+    seqan::CharString tmpDir;
+// Parameter used during the RNAfold execution to select the minimum energy to be considered
+    double thrBppm;
+// define weight of first found edge detected in fixed graphs
+    double firstEdgeWeight;
+// define the weight of more edges detected  in fixed graphs
+    double edgeStepWeight;
+// time used for an hard timeout
+    unsigned timeLimit;
+// verbose(0) no outputs,
+// verbose(1) Displays global statistics,
+// verbose(2) Displays extensive statistics for each batch of reads,
+// verbose(3) Debug output.
+    unsigned verbose;
+// number of threads forced
+    unsigned threads;
+// number of threads detected
+    unsigned threadsCount;
+    Options() :
+            thrBppm(1e-15), // 0.1 is the value used in the old Lara
+            firstEdgeWeight(1.0),
+            edgeStepWeight(0.5),
+            timeLimit(-1),
+            verbose(0)
+    {
+//#ifdef _OPENMP
+//        threadsCount = 1; // TODO fix and implement multithreading -> std::thread::hardware_concurrency(); // omp_get_num_threads()
+//        threads = threadsCount;
+//#else
+//        threadsCount = 1;
+//#endif
+    }
+};
+
+// ============================================================================
+// Functions
+// ============================================================================
+
+using namespace seqan;
+// ----------------------------------------------------------------------------
+// Function setupArgumentParser()
+// ----------------------------------------------------------------------------
+template <typename TOption>
+void setupArgumentParser(ArgumentParser & parser, TOption const & /* options */)
+{
+    setAppName(parser, "CoSMo");
+    setShortDescription(parser, "Consensus Structure Module for filtering Base Pair Probability Matrix of RNA secondary structure");
+    setCategory(parser, "Optimizer of RNA BPP matrix");
+    setVersion(parser, "1.0");
+    setDate(parser, "2017");
+    //setDateAndVersion(parser);
+    //setDescription(parser);
+    addUsageLine(parser, "./cosmo <\\fI-i inFile\\fP> \
+            [\\fI-w outFile\\fP] [\\fI -parameters\\fP]");
+    addOption(parser, ArgParseOption("v", "verbose", "verbose(0) no outputs, verbose(1) Displays global statistics, "
+            "verbose(2) Displays extensive statistics for each batch of reads, verbose(3) Debug output.",
+            ArgParseArgument::INTEGER, "INT"));
+
+    addSection(parser, "CoSMo Options");
+    addOption(parser, ArgParseOption("tb", "thrBppm", "(Parameter used during the RNAfold execution to select the "
+            "minimum energy to be considered (default: 1e-15) ", ArgParseArgument::DOUBLE, "DOUBLE"));
+    addOption(parser, ArgParseOption("few", "firstEdgeWeight", "define weight of first found edge detected in fixed graphs",
+                                     ArgParseArgument::DOUBLE, "DOUBLE"));
+    addOption(parser, ArgParseOption("esw", "edgeStepWeight", "define the weight of more edges detected  in fixed graphs",
+                                     ArgParseArgument::DOUBLE, "DOUBLE"));
+
+    addSection(parser, "Input Options");
+    addOption(parser, ArgParseOption("i", "inFile", "Path to the input file", ArgParseArgument::INPUT_FILE, "IN"));
+//    addOption(parser, ArgParseOption("ir", "inFileShape", "Path to the reference shape file",
+//                                     ArgParseArgument::INPUT_FILE, "IN"));
+
+    addSection(parser, "Output Options");
+    addOption(parser, seqan::ArgParseOption( "w", "outFile", "Path to the output file (default: stdout)",
+                                             ArgParseArgument::OUTPUT_FILE, "OUT"));
+    addOption(parser, ArgParseOption("td", "tmpDir", "Specify a temporary directory where to save intermediate files. \
+            Default: use the input file directory.", ArgParseOption::STRING));
+    // Setup performance options.
+    addSection(parser, "Performance Options");
+    addOption(parser, ArgParseOption("t", "threads", "Specify the number of threads to use.", ArgParseOption::INTEGER));
+    setMinValue(parser, "threads", "1");
+//#ifdef _OPENMP
+//    setMaxValue(parser, "threads", "1") // TODO fix the multithreading -> std::to_string(std::thread::hardware_concurrency() + 1));
+//#else
+ //   setMaxValue(parser, "threads", "1");
+//#endif
+//    setDefaultValue(parser, "threads", options.threadsCount);
+}
+
+// ----------------------------------------------------------------------------
+// Function setEnv()
+// ----------------------------------------------------------------------------
+
+template <typename TString, typename TValue>
+bool setEnv(TString const & key, TValue const & value)
+{
+#ifdef PLATFORM_WINDOWS
+    return !_putenv_s(toCString(key), toCString(value));
+#else
+    return !setenv(toCString(key), toCString(value), true);
+#endif
+}
+
+// ----------------------------------------------------------------------------
+// Function getCwd()
+// ----------------------------------------------------------------------------
+
+template <typename TString>
+void getCwd(TString & string)
+{
+    char cwd[1000];
+
+#ifdef PLATFORM_WINDOWS
+    _getcwd(cwd, 1000);
+#else
+    ignoreUnusedVariableWarning(getcwd(cwd, 1000));
+#endif
+
+    assign(string, cwd);
+}
+
+// ----------------------------------------------------------------------------
+// Function firstOf()
+// ----------------------------------------------------------------------------
+
+template <typename TString, typename TFunctor>
+inline typename Iterator<TString const, Standard>::Type
+firstOf(TString const & string, TFunctor const & f)
+{
+    typedef typename Iterator<TString const, Standard>::Type TIter;
+
+    TIter it = begin(string, Standard());
+    skipUntil(it, f);
+
+    return it;
+}
+
+// ----------------------------------------------------------------------------
+// Function lastOf()
+// ----------------------------------------------------------------------------
+
+template <typename TString, typename TFunctor>
+inline typename Iterator<TString const, Standard>::Type
+lastOf(TString const & string, TFunctor const & f)
+{
+    typedef ModifiedString<TString const, ModReverse>        TStringRev;
+    typedef typename Iterator<TStringRev, Standard>::Type    TIterRev;
+
+    TStringRev revString(string);
+    TIterRev revIt = firstOf(revString, f);
+
+    return end(string) - position(revIt, revString);
+}
+
+// ----------------------------------------------------------------------------
+// Function getPath()
+// ----------------------------------------------------------------------------
+
+template <typename TString>
+inline typename Prefix<TString const>::Type
+getPath(TString const & string)
+{
+    typedef typename Iterator<TString const, Standard>::Type TIter;
+
+    TIter it = lastOf(string, IsPathDelimited());
+
+    if (it != begin(string, Standard())) --it;
+
+    return prefix(string, it);
+}
+
+// ----------------------------------------------------------------------------
+// Function parseCmd()
+// ----------------------------------------------------------------------------
+template <typename TOption>
+ArgumentParser::ParseResult parse(TOption & options, ArgumentParser & parser, int argc, char const ** argv)
+{
+    ArgumentParser::ParseResult res = parse(parser, argc, argv);
+    if (res != ArgumentParser::PARSE_OK)
+        return res;
+    getOptionValue(options.verbose, parser, "verbose");
+    getOptionValue(options.thrBppm, parser, "thrBppm");
+
+    getOptionValue(options.firstEdgeWeight, parser, "firstEdgeWeight");
+    getOptionValue(options.edgeStepWeight, parser, "edgeStepWeight");
+    getOptionValue(options.threads, parser, "threads");
+
+    getOptionValue(options.inFile, parser, "inFile");
+    if (empty(options.inFile))
+        return ArgumentParser::PARSE_ERROR;
+
+//    getOptionValue(options.inFileRef, parser, "inFileRef");
+    getOptionValue(options.outFile, parser, "outFile");
+    if (isSet(parser, "outFile"))
+    {
+        _V(options, "The specified output file is " << options.outFile);
+    }
+    else
+    {
+        CharString tmpDir;
+        getOptionValue(tmpDir, parser, "tmpDir");
+        if (!isSet(parser, "tmpDir"))
+        {
+            tmpDir = SEQAN_TEMP_FILENAME();
+            // remove "/test_file" suffix
+            erase(tmpDir, length(tmpDir) - 10u, length(tmpDir));
+        }
+        setEnv("TMPDIR", tmpDir);
+        options.tmpDir = tmpDir;
+        _V(options, "The absolute path where to create the tmpDir is " << tmpDir);
+    }
+//    showScoringMatrix(options.laraScoreMatrix);
+    return ArgumentParser::PARSE_OK;
+}
+
+#endif /* INCLUDE_OPTION_SRTUCT_FUNCT_H_ */
--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@
 #include <vector>
 #include <sstream>
 #include <cstdio>
+#include <limits>
 clock_t start = clock();
+
+// ----------------------------------------------------------------------------
+// SeqAn headers
+// ----------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,24 +29,68 @@ clock_t start = clock();
 #include <seqan/graph_algorithms.h>
 #include <seqan/rna_io.h>
 
+// ----------------------------------------------------------------------------
+// App headers
+// ----------------------------------------------------------------------------
+
+// defines all the constants used in the app
+//#include "data_types.h"
+#include "option.h"
+
 using namespace seqan;
 
-int main () {
-    std::string line;
-
-    //tools that you want to use
-
-    std::string tool_name_1("RNAfold");
-    std::string tool_name_2("RME");
-    std::string tool_name_3("RNAstructure");
-    std::string tool_name_4("SeqFold");
-
-    std::ifstream myfile("../data/config_file.txt");
-    if (!(myfile.is_open()))       // reading a configuration file
+int main (int argc, char const ** argv)
     {
-        std::cout << "Unable to open configuration file";
-        exit (EXIT_FAILURE);
-    }
+        // Argument Parser
+        if (argc == 1)
+        {
+            std::cout << "type " << argv[0] << " --help to get the parameters table (-i option is mandatory)" << std::endl;
+            return 1;
+        }
+
+        ArgumentParser parser;
+        Options options;
+        setupArgumentParser(parser, options);
+        ArgumentParser::ParseResult res;
+        res = parse(options, parser, argc, argv); // Fill the options structure
+        if (res != ArgumentParser::ParseResult::PARSE_OK)
+            return res == ArgumentParser::ParseResult::PARSE_ERROR ? 1 : 0;
+
+        // Read input files
+
+        RnaStructFileIn inputfile;                             //This inputfile is the output of the second tool
+        if (!open(inputfile, toCString(options.inFile), OPEN_RDONLY))
+            // if (!open(inputfile, "1out_Human_tRNA_38.chr6.fold", OPEN_RDONLY))
+        {
+            std::cerr << "could not open inputfile" << std::endl;
+            return 1;
+        }
+
+        RnaStructFileOut outputfile;
+        if (!open(outputfile, "../data/output.ebpseq", OPEN_RDWR)) {
+            std::cerr << "could not open outputfile" << std::endl;
+            return 1;
+        }
+
+        RnaStructContents contents;
+        readRecords(contents, inputfile, std::numeric_limits<unsigned>::max());
+        _VVV(options, contents.header.description);
+        for(unsigned i = 0; i < length(contents); ++i)
+        {
+            _VVV(options, "iteration i = " << i);
+            _VVV(options, contents.records[i].sequence);
+            _VVV(options,contents.records[i].name);
+            for(unsigned j = 0; j < length(contents.records[i].fixedGraphs); ++j)
+            {
+                _VVV(options, "iteration j = " << j);
+                _VVV(options, contents.records[i].fixedGraphs[j].inter);
+                _VVV(options, contents.records[i].fixedGraphs[j].specs);
+                _VVV(options, contents.records[i].fixedGraphs[j].energy);
+            }
+        }
+
+        return 167;
+//        _readRnaInputFile(filecontents1, options.inFile, options);
 
     //////////////////////////////////////////////////////////////////////////////////////////////
     //Run the line of the configuration file
@@ -80,51 +129,27 @@ int main () {
 //        return 1;
 //    }
 
-    RnaStructFileIn inputfile;
-    if (!open(inputfile, "../data/in.dbn", OPEN_RDONLY))            //This inputfile is the output of the first tool
-        // if (!open(inputfile, "1out_Human_tRNA_38.chr6.fold", OPEN_RDONLY))
-    {
-        std::cerr << "could not open inputfile" << std::endl;
-        return 1;
-    }
-    RnaStructFileIn inputfile2;                             //This inputfile is the output of the second tool
-    if (!open(inputfile2, "../data/in2.dbn", OPEN_RDONLY))
-        // if (!open(inputfile, "1out_Human_tRNA_38.chr6.fold", OPEN_RDONLY))
-    {
-        std::cerr << "could not open inputfile" << std::endl;
-        return 1;
-    }
-
-    RnaStructFileOut outputfile;
-    if (!open(outputfile, "../data/output.ebpseq", OPEN_RDWR)) {
-        std::cerr << "could not open outputfile" << std::endl;
-        return 1;
-    }
-
-    //one record of RnaStructContents is a sequence
-    RnaStructContents contents{};
-    RnaStructContents contents2{};
-    readRecords(contents, inputfile, 100);
-    readRecords(contents2, inputfile2, 100);
 
     //////////////////////////////////////////////////////////////////////////////////////////////
     //Compare sequences
     //////////////////////////////////////////////////////////////////////////////////////////////
     //Compare the sequnces of the files obtained by using the different tools
     //If the sequence is the same, the sequence is written in an output file.
+    RnaStructContents contents2;
 
     if (contents.records[0].sequence== contents2.records[0].sequence) {
         writeRecords(outputfile, contents);          //AFTER -APPEND FUNCTION- THE OUTPUT FILE IS EMPTY.NO WRITTEN RECORDS.NO ERRORS
         std::cout << "The sequence of the files obtained by the tools is the same." << std::endl;
         std::cout << "Sequence: " <<contents.records[0].sequence << std::endl;
         std::cout << "Length: " <<contents.records[0].seqLen << std::endl;
+        std::cout << "-> Sequence position access: " << contents.records[0].sequence[0] << std::endl;
         //std::cout << "Graph in 0 position of the FixedGraph String: " << contents.records[0].fixedGraphs[0];
     }
     else {
         std::cout << "The sequence if the files is not the same." << std::endl;
         exit (EXIT_FAILURE);
     }
-
+    return 0;
     //////////////////////////////////////////////////////////////////////////////////////////////
     //Definition of null objects
     //////////////////////////////////////////////////////////////////////////////////////////////
@@ -146,114 +171,39 @@ int main () {
             //std::cout << vett[m] << " ";
     }
 
-    std::cout << std::endl << "NOTE: This Adjacency list results to be different from the one written in the output.ebpseq file (row 117) ";
     std::cout << std::endl << contents.records[0].fixedGraphs[0].inter;
 
-    //////////////////////////////////////////////////////////////////////////////////////////////
-    //Creation of the matrix with the base-pairs and the respoctive frequences
-    //////////////////////////////////////////////////////////////////////////////////////////////
-
-        //These will be the arrays obtained in the previous part
-//        int RNAfold_vett[contents.records[0].seqLen];            /////////////////////////////////////////////////////////////////////
-//        int RME_vett[contents.records[0].seqLen];                /////////////////////////////////////////////////////////////////////
-//        int RNAstructure_vett[contents.records[0].seqLen];       /////////////////////////////////////////////////////////////////////
-//        int SeqFold_vett[contents.records[0].seqLen];            /////////////////////////////////////////////////////////////////////
-
-        //matrix[n][m]: n->sequnce length m->(number of tools=4) x 2-> in order to have for each possible value the frequence
-//        int matrix[contents.records[0].seqLen][8];
-
-    int RME_flag=0;
-    int RNAstructure_flag=0;
-    int SeqFold_flag=0;
-
-    int RNAfold_count=1;
-    int RME_count=1;
-    int RNAstructure_count=1;
-    int SeqFold_count=1;
-
     /////////////////////////////////////////////////////////////////////
-    //Examples of arrays obtained by accessing the graphs
-    //The length of the sequence in this case is 3.
+    //OutputGraph creation by copying the first one
     /////////////////////////////////////////////////////////////////////
-    int RNAfold_vett[3]={5, 6, 7};
-    int RME_vett[3]={9,6,7};
-    int RNAstructure_vett[3]={9, 11, 7};
-    int SeqFold_vett[3]={5, 11, 7};
-    int matrix[3][8];
-    /////////////////////////////////////////////////////////////////////
+/*
+    typedef unsigned int TCargo;
+    typedef Graph<Undirected<TCargo> > TGraph;
+    typedef VertexDescriptor<TGraph>::Type TVertexDescriptor;
 
- //       for (int n=0; n<contents.records[0].seqLen; n++) {                          /////////////////////////////////////////////////////////////////////
-    for (int n=0; n<3; n++) {
-        int m = 0;
-        matrix[n][m] = RNAfold_vett[n];
-        if (RNAfold_vett[n] == RME_vett[n]) {
-            RNAfold_count++;
-            RME_flag = 1;
-        } else {
-            m = m + 2;
-            matrix[n][m] = RME_vett[n];
-            if (RME_vett[n]==RNAstructure_vett[n]) {
-                RME_count++;
-                RNAstructure_flag=1;
-            }
-            if (RME_vett[n]==SeqFold_vett[n]) {
-                RME_count++;
-                SeqFold_flag=1;
-            }
-            matrix[n][m+1] = RME_count;
-        }
-        if (RNAfold_vett[n] == RNAstructure_vett[n]) {
-            RNAfold_count++;
-            RNAstructure_flag = 1;
-        } else if (RNAstructure_flag != 1) {
-            m = m + 2;
-            matrix[n][m] = RNAstructure_vett[n];
-            if (RNAstructure_vett[n]==SeqFold_vett[n]) {
-                RNAstructure_count++;
-                SeqFold_flag=1;
-            }
-            matrix[n][m+1] = RNAstructure_count;
-        }
-        if (RNAfold_vett[n] == SeqFold_vett[n]) {
-            RNAfold_count++;
-            SeqFold_flag = 1;
-        } else if (SeqFold_flag != 1 ){
-            m = m + 2;
-            matrix[n][m] = SeqFold_vett[n];
-            matrix[n][m+1] = SeqFold_count;
-        }
-        matrix[n][1] = RNAfold_count; ///////////////////////////////////////
+    TGraph OutputGraph;
 
-        std::cout << "> " << n+1 << " nucleotide : " << std::endl;
-        std::cout << "RNAfold COUNT: " << RNAfold_count << ",  ";
-        std::cout << "RME COUNT: " << RME_count << ",  ";
-        std::cout << "RNAstructure COUNT: " << RNAstructure_count << ",  ";
-        std::cout << "SeqFold COUNT: " << SeqFold_count << std::endl;
-
-        RNAfold_count = 1;
-        RME_count = 1;
-        RNAstructure_count = 1;
-        SeqFold_count = 1;
-
-        RME_flag = 0;
-        RNAstructure_flag = 0;
-        SeqFold_flag = 0;
+    for (int i=0; i<contents.records[0].seqLen; i++)    {
+        TVertexDescriptor contents.records[0].sequence[i] = addVertex(OutputGraph);
+    }
+    for (int i=0; i<contents.records[0].seqLen; i++)     {
+        addEdge(OutputGraph, , , );
     }
 
-    std::cout << "MATRIX:" << std::endl;
-    for (int j=0; j<3; j++){
-        for (int k=0; k<8; k++){
-            std::cout << matrix[j][k] << " ";
-        }
-        std::cout << std::endl;
-    }
+
+    */
+
+    /////////////////////////////////////////////////////////////////////
+    //Updating of the output graph by accessing to the others(if the edge exists->updating of the weight, if it does not exists->creation of the edge)
+    /////////////////////////////////////////////////////////////////////
+
     /////////////////////////////////////////////////////////////////////
     //SHAPE
     /////////////////////////////////////////////////////////////////////
     //SHAPE data contribution to have the right weights
 
     /////////////////////////////////////////////////////////////////////
-    //Graph
+    //Filtering of the bpp matrix by using the output graph
     /////////////////////////////////////////////////////////////////////
 
 //Example:
@@ -265,7 +215,6 @@ int main () {
 //    addEdge(bgraph.inter, 1u, 2u, 0.2);
 //    append(contents.records[0].bppMatrGraphs, bgraph);
 
-    myfile.close();
 
     clock_t end = clock();
     double time = (double) (end-start) / CLOCKS_PER_SEC * 1000.0;
